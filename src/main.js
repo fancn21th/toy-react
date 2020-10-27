b687160bf83419fc056f9fada425ccf5ab732341
范轶洁 <fanyjb@digitalchina.com>
2020-10-27 10:08:09 +0800

    class render

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
-const a = <div>hello world!</div>;
+import { createElement, render, Component } from "./toy-react";
+
+class Foo extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        {this.children}
+      </div>
+    );
+  }
+}
+
+render(
+  <Foo id="foo">
+    <h1>hello world</h1>
+    <h1>hello world</h1>
+    <h1>hello world</h1>
+  </Foo>,
+  document.body
+);
